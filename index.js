b876ec8ae5fc12542965de8453044cec3a451db0
Erick Vargas <erickkvargas@gmail.com>
2022-04-15 14:47:39 -0700

    commit tree d61f7f606a9052ef3af527deb49cdd13029007c1

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email){
+return fetch("http://localhost:3000/users", {
+    method: "POST", 
+ headers: {
+    "Content-type": "application/json", 
+     "Accept": "application/json",
+ },
+ body: JSON.stringify({
+     name,
+     email 
+ })
+ })
+.then(response => response.json())
+.then(data => document.body.innerHTML = data["id"])
+.catch(error => document.body.innerHTML = error.message)
+
+// .then(response=> response.json())
+// .then(data=>{console.log(data)})
+// .catch(error => alert("uh oh") )
+
+
+}
+
+// {method: "POST", 
+// headers: {
+//     "Content-Type": "application/json", 
+//     Accept: "application/json",
+// },
+// body: JSON.stringify(name, email)
+// }
